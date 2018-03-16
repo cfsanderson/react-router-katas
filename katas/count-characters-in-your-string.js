d26bb76760ba786cfa9b9ca9e61be535946e9898
cfsanderson <calebsanderson@gmail.com>
2018-03-16 09:55:52 -0400

    Adds katas directory and current kata in progress

---
+++ katas/count-characters-in-your-string.js
@@ -0,0 +1,13 @@
+function count (string) {
+  let newObj = {}
+
+if (string.length === 0) {
+    return newObj
+  } else {
+    let sort = string.split("").reduce()
+    for (let i = 0; i < sort.length; i++) {
+      console.log(sort)
+    }
+  }     
+}
+// string.length === 0 ? newObj : console.log(string.length)
