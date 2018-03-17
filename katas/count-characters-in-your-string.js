b200da9880ee54e1b73f9141f281329d0d7fa4dd
cfsanderson <calebsanderson@gmail.com>
2018-03-17 19:35:24 -0400

    Updates count characters in your string

--- katas/count-characters-in-your-string.js
+++ katas/count-characters-in-your-string.js
@@ -1,15 +1,39 @@
-function count (string) {
-  let newObj = {}
 
-if (string.length === 0) {
-    return newObj
+
+// function count (potato) {
+//   // make an empty object
+//   let newObj = {}
+
+//   // check to see if the string is empty, if so return the empty object
+//   if (potato.length === 0) {
+//     return newObj
+//   } else {
+//     // 
+//     let sort = potato.split("")
+//     for (let i = 0; i < sort.length; i++) {
+      
+//       console.log(sort[i])
+//     }
+//   }
+// }
+let string = "aabba"
+
+function count(string) {
+  let arr = []
+  if (string.length === 0) {
+    return {}
   } else {
-    let sort = string.split("").reduce()
+    let sort = string.split("")
     for (let i = 0; i < sort.length; i++) {
-      console.log(sort)
+      if (sort[i] === sort[i+1]) {
+        arr.push(sort[i])
+      }
     }
-  }     
+  }
+  console.log(arr)
 }
+
+count(string);
 // string.length === 0 ? newObj : console.log(string.length)
 //
 // https://www.codewars.com/kata/count-characters-in-your-string/train/javascript
